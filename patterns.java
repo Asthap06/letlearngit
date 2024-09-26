2cbfbd0767cdc8b0821f267f0fb8ec638fcefa7b
Astha Pandey <164369166+Asthap06@users.noreply.github.com>
2024-09-26 17:34:48 +0530

    next pattern

--- patterns.java
+++ patterns.java
@@ -1,17 +1,33 @@
-// code for the patterns in java:
-import java.util.*;
+// // code for the patterns in java:
+// import java.util.*;
+// public class patterns{
+//     public static void printpattern(int n){
+//         int i,j;
+//         for(i=1; i<=n; i++){
+//             for(j=1; j<=i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+//     public static void main(String args[]){
+//         int n=5;
+//         printpattern(n);
+//     }
+// }
+// next pattter
 public class patterns{
     public static void printpattern(int n){
-        int i,j;
-        for(i=1; i<=n; i++){
-            for(j=1; j<=i; j++){
-                System.out.print("*");
+        int x,y;
+        for(x=1 ;x<=n; x++){
+            for(y=1; y<=x; y++){
+                System.out.print(y + " ");
             }
             System.out.println();
         }
     }
-    public static void main(String args[]){
-        int n=5;
+    public static void main(String[] args) {
+        int n=6;
         printpattern(n);
     }
 }
